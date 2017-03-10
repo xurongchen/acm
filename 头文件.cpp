14c1c6585a33e71614a14b8568227161f096e2e6
xurongchen <1459572152@qq.com>
2017-03-10 12:27:46 +0800

    AC自动机

---
+++ 头文件.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<fstream>
+#include<cstring>
+#include<string>
+#include<set>
+#include<map>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<cmath>
+#include<list>
+#define INF 0X3F3F3F3F
+#define MOD 1000000007
+#define LL long long
+#define SC(x) scanf("%d",&x)
+#define SCC(x,y) scanf("%d%d",&x,&y)
+#define SCCC(x,y,z) scanf("%d%d%d",&x,&y,&z)
+#define SS(x) scanf("%s",x)
+#define PR(x) printf("%d\n",x)
+
+int main()
+{
+//    freopen("in.txt","r",stdin);
+//    freopen("out.txt","w",stdout);
+
+
+
+}
